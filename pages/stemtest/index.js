b82f7467dfacb9631eef702cdcef7257650f192d
Kaat Vermeire <kaat.vermeire@student.howest.be>
2021-06-10 15:09:10 +0200

    add html to stemtest start #19

--- pages/stemtest/index.js
+++ pages/stemtest/index.js
@@ -1,6 +1,17 @@
+import styles from "./Stemtest.module.css";
+
 export default function Stemtest () {
 
     return (
-        <h1>STEMTEST</h1>
+        <article className={styles.introduction}>
+            <h1 className={styles.title}>STEMTEST</h1>
+            <p className={styles.intro_bold}>In welke mate ben jij akkoord met bepaalde standpunten van politieke partijen?</p>
+            <p className={styles.intro_light}>Ben je zeker dat je alle correcte informatie hebt om kritische keuzes te maken?</p>
+
+            <div className={styles.input}>
+                <input className={styles.range} type="range"></input>
+            </div>
+        </article>
+        
     )
 }
