4ed61c3abf24d3d6d2f7e9cec7488341d35301f8
Amberas <amber.aspeslagh@student.howest.be>
2021-06-10 14:54:06 +0200

    background stemtest #24

---
+++ pages/stemtest.js
@@ -0,0 +1,12 @@
+import styles from '../styles/Stemtest.module.css'
+
+export default function stemtest() {
+    return (
+        <>
+            <section className={styles.container}>
+               
+            </section>
+        </>
+    )
+
+}
