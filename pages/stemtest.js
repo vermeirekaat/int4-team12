dc90e455568fecc8af2e79cd91d1b21ace0a50f4
Amberas <amber.aspeslagh@student.howest.be>
2021-06-10 19:51:34 +0200

    responsive eerste vraag #23

--- pages/stemtest.js
+++ pages/stemtest.js
@@ -1,7 +1,7 @@
 import styles from '../styles/Stemtest.module.css';
 import { useState } from "react";
 import Image from "next/image";
-import Button from '../components/stemtest/Button'
+import Button from '../components/stemtest/Button';
 
 export default function stemtest() {
 
@@ -11,6 +11,8 @@ export default function stemtest() {
             "picture": "/assets/hoofdoek.png",
             "question": <p className={styles.text}>Ik vind dat het personeel van de <span className={styles.highlight}>Vlaamse overheid</span> achter het loket een  <span className={styles.highlight}>hoofddoek</span> mag dragen.</p>,
             "input": <div className={styles.buttons}><Button text="Eens" /><Button text="Oneens" /></div>,
+            "imageClassName": styles.hoofdoek,
+            "bigImageClassName": styles.hoofdoek_image
         },
     ];
 
@@ -21,14 +23,22 @@ export default function stemtest() {
             <section className={styles.container}>
                 <div className={styles.background}></div>
                 <div>
-                    <img className={styles.versiering1} src="/assets/versiering.png" width={16} height={23}/>
+                    <img className={styles.versiering1} src="/assets/versiering.png" width={16} height={23} />
                     <img className={styles.versiering2} src="/assets/versiering 2.png" width={19} height={20} />
                     <img className={styles.versiering3} src="/assets/versiering 3.png" width={23} height={24} />
                 </div>
+                <div className={question.bigImageClassName}></div>
                 <div className={styles.content}>
-                    <Image src={question.picture} width={416} height={542} />
-                    {question.question}
-                    {question.input}
+                    <div className={styles.progress_div}>
+                        <div className={styles.progress} style={{ width: (question.questionNumber * 10) + 'vw' }}> </div>
+                    </div>
+                    <div className={styles.content_container}>
+                        <img className={question.imageClassName} src={question.picture} />
+                        <div className={styles.buttons_text}>
+                            {question.question}
+                            {question.input}
+                        </div>
+                    </div>
                 </div>
             </section>
         </>
