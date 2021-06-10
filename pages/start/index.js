3be49583ae08cca5e9b4fe5af851ebbfe474c368
Kaat Vermeire <kaat.vermeire@student.howest.be>
2021-06-10 15:48:28 +0200

    add css for mobile #19

--- pages/start/index.js
+++ pages/start/index.js
@@ -1,15 +1,29 @@
-import styles from "./Stemtest.module.css";
+import styles from "./Start.module.css";
+import Image from "next/image";
 
 export default function Stemtest () {
 
     return (
-        <article className={styles.introduction}>
-            <h1 className={styles.title}>STEMTEST</h1>
+        <article className={styles.container}>
+            <div className={styles.title_container}>
+                <p className={styles.title_stroke}>Stemtest</p>
+                <p className={styles.title_stroke}>Stemtest</p>
+                <p className={styles.title_stroke}>Stemtest</p>
+                <h1 className={styles.title_fill}>Stemtest</h1>
+            </div>
             <p className={styles.intro_bold}>In welke mate ben jij akkoord met bepaalde standpunten van politieke partijen?</p>
             <p className={styles.intro_light}>Ben je zeker dat je alle correcte informatie hebt om kritische keuzes te maken?</p>
 
+            <div className={styles.image}>
+                <Image
+                    src="/assets/protest.png"
+                    alt="Protest"
+                    width={350}
+                    height={250}/>
+            </div>
+
             <div className={styles.input}>
-                <input className={styles.range} type="range"></input>
+                <input className={styles.range} type="range" defaultValue="0"></input>
             </div>
         </article>
         
