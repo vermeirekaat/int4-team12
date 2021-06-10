599cb8ef4ebbbbd40c6edd44f849a315379bee4a
Kaat Vermeire <kaat.vermeire@student.howest.be>
2021-06-10 23:21:12 +0200

    add images #20

--- pages/start/index.js
+++ pages/start/index.js
@@ -1,6 +1,7 @@
 import styles from "./Start.module.css";
 import Metadata from "../../components/Metadata";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Stemtest () {
 
@@ -17,11 +18,26 @@ export default function Stemtest () {
             <p className={styles.intro_first}>In welke mate ben jij akkoord met bepaalde standpunten van politieke partijen?</p>
             <p className={styles.intro_second}>Ben je zeker dat je alle correcte informatie hebt om kritische keuzes te maken?</p>
 
-            <div className={styles.image}>
-                {/*<Image
-                    src="/assets/protest.png"
-                    alt="Protest"
-                    layout="fill"/>*/}
+            <div className={styles.input_desktop}>
+                <Link href="/stemtest">
+                    <a className={styles.button}> Click to get started</a>
+                </Link>
+            </div>
+
+            <div className={styles.images}>
+                <div className={styles.image_left}>
+                    {/*<Image
+                        src="/assets/protest.png"
+                        alt="Protest"
+                        layout="fill"/>*/}
+                </div>
+                <div className={styles.image_right}>
+                    <Image
+                        src="/assets/circles.png"
+                        alt="Protest"
+                        width={357}
+                        height={598}/>
+            </div>
             </div>
 
             <div className={styles.input_mobile}>
@@ -29,9 +45,9 @@ export default function Stemtest () {
                 <input className={styles.range} type="range" defaultValue="0"></input> 
             </div>
 
-            <div className={styles.input_desktop}>
-                <p className={styles.button}>Click to get started</p>
-            </div>
+            <div className={styles.image_extra}></div>
+            <div className={styles.image_double}></div>
+            <div className={styles.image_triple}></div>
         </article>
         </>     
     )
