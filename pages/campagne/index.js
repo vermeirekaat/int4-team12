a2fda8999fc10bfa56ff4cb9bda9ce08d6ca86a3
Kaat Vermeire <kaat.vermeire@student.howest.be>
2021-06-14 19:19:07 +0200

    responsive images on header #34

--- pages/campagne/index.js
+++ pages/campagne/index.js
@@ -14,7 +14,7 @@ export default function Campagne () {
                 <div className={styles.introduction}>
                     <div className={styles.window_green}></div>
                     <p className={styles.paragraaf}>
-                        Je spendeert talloze minuten op sociale media (Facebook, Instagram...) waarbij honderden berichten jouw aandacht vragen. Maar op welke manier wordt er beslist welke berichten JIJ te zien krijgt? <br/> <br/> Het zijn je eigen kliks die bepalen welke berichten er op je feed terecht komen.
+                        Je spendeert talloze minuten op sociale media (Facebook, Instagram...) waarbij honderden berichten jouw aandacht vragen. Maar op welke manier wordt er beslist welke berichten JIJ te zien krijgt? <br/> <br/> Het zijn je eigen clicks die bepalen welke berichten er op je feed terecht komen.
                     </p>
                 </div>
                 <div className={styles.image_protest}>
