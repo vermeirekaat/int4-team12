4e49ef2e56505fbc1b83f4a81b19592f448d7b38
Eline Claeys <eline.claeys4@student.howest.be>
2021-06-16 10:48:26 +0200

    img tags naar next/image veranderd close #39

--- pages/start/index.js
+++ pages/start/index.js
@@ -6,7 +6,7 @@ import { useRouter } from "next/router";
 
 
 
-export default function Stemtest () {
+export default function Stemtest() {
     const router = useRouter();
 
     const handleRangeInput = (e) => {
@@ -17,44 +17,44 @@ export default function Stemtest () {
 
     return (
         <>
-        <Metadata/>
-        <article className={styles.container}>
-            <div className={styles.title_container}>
-                <p className={styles.title_stroke}>Stemtest</p>
-                <p className={styles.title_stroke}>Stemtest</p>
-                <p className={styles.title_stroke}>Stemtest</p>
-                <h1 className={styles.title_fill}>Stemtest</h1>
-            </div>
-            <p className={styles.intro_first}>In welke mate ben jij akkoord met bepaalde standpunten van politieke partijen?</p>
-            <p className={styles.intro_second}>Ben je zeker dat je alle correcte informatie hebt om kritische keuzes te maken?</p>
-
-            <div className={styles.input_desktop}>
-                <Link href="/stemtest">
-                    <a className={styles.button}> Click to get started</a>
-                </Link>
-            </div>
-
-            <div className={styles.images}>
-                <div className={styles.image_left}>
+            <Metadata />
+            <article className={styles.container}>
+                <div className={styles.title_container}>
+                    <p className={styles.title_stroke}>Stemtest</p>
+                    <p className={styles.title_stroke}>Stemtest</p>
+                    <p className={styles.title_stroke}>Stemtest</p>
+                    <h1 className={styles.title_fill}>Stemtest</h1>
                 </div>
-                <div className={styles.image_right}>
-                    <Image
-                        src="/assets/circles.png"
-                        alt="Protest"
-                        width={357}
-                        height={598}/>
-            </div>
-            </div>
+                <p className={styles.intro_first}>In welke mate ben jij akkoord met bepaalde standpunten van politieke partijen?</p>
+                <p className={styles.intro_second}>Ben je zeker dat je alle correcte informatie hebt om kritische keuzes te maken?</p>
 
-            <div className={styles.input_mobile}>
-                <p className={styles.instruction}>Slide to get started</p>
-                <input className={styles.range} onChange={(e) => handleRangeInput(e)} type="range" defaultValue="0"></input> 
-            </div>
+                <div className={styles.input_desktop}>
+                    <Link href="/stemtest">
+                        <a className={styles.button}> Click to get started</a>
+                    </Link>
+                </div>
 
-            <div className={styles.image_extra}></div>
-            <div className={styles.image_double}></div>
-            <div className={styles.image_triple}></div>
-        </article>
-        </>     
+                <div className={styles.images}>
+                    <div className={styles.image_left}>
+                    </div>
+                    <div className={styles.image_right}>
+                        <Image
+                            src="/assets/circles.png"
+                            alt="Protest"
+                            width={357}
+                            height={598} />
+                    </div>
+                </div>
+
+                <div className={styles.input_mobile}>
+                    <p className={styles.instruction}>Slide to get started</p>
+                    <input className={styles.range} onChange={(e) => handleRangeInput(e)} type="range" defaultValue="0"></input>
+                </div>
+
+                <div className={styles.image_extra}></div>
+                <div className={styles.image_double}></div>
+                <div className={styles.image_triple}></div>
+            </article>
+        </>
     )
 }
