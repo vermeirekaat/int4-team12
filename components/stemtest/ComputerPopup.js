1a8dfb8d5a8308362aa4905dddfad001aa5273d1
Amberas <amber.aspeslagh@student.howest.be>
2021-06-16 13:14:35 +0200

    mouseoverload zonder position #32

--- components/stemtest/ComputerPopup.js
+++ components/stemtest/ComputerPopup.js
@@ -1,12 +1,38 @@
 import styles from './ClosePopup.module.css';
 import Button from './ButtonPurple';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
+import { useState } from "react";
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function ClosePopup({ onClicked}) {
+    const [mouses, setMouses] = useState([]);
+
+    const mouse = (e) => {
+        let mouse;
+
+        console.log(e);
+
+        const x = e.pageX + 10;
+        const y = e.pageY + 10;
+
+        mouse = React.createElement(
+            "img", {
+            src: "./assets/campagne/pijl.png",
+            className: "mouse",
+            //style: `position:absolute; left:${x}px;top:${y}px`,
+            key: uuidv4()
+        })
+
+        setMouses(mouses.concat(mouse));
+
+        //deleteFirst();
+
+    }
+
     return (
         <>
-            <div className={styles.close_container}>
+            <div className={styles.close_container} onMouseMove={e => mouse(e)}>
+                {mouses}
                 <div className={styles.box}>
                     <span onClick={onClicked} className={styles.close}></span>
                     <p className={styles.text}>U gaat akkoord met de stelling, volgens de computer.</p>
