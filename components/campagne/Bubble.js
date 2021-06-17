16074799b09b672109ede788d878fcd944288f01
Kaat Vermeire <kaat.vermeire@student.howest.be>
2021-06-17 16:32:10 +0200

    animations -finish #44

--- components/campagne/Bubble.js
+++ components/campagne/Bubble.js
@@ -2,6 +2,7 @@ import Metadata from "../Metadata";
 import styles from "./Bubble.module.css";
 import Image from "next/image";
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 export default function Bubble ({ input }) {
     
@@ -13,15 +14,31 @@ export default function Bubble ({ input }) {
           <article className={styles.finish}>
               <div className={styles.finish_image}></div>
               <div className={styles.finish_content}>
-                  <h1 className={styles.title}>{input.title}</h1>
-                  <p className={styles.paragraaf}>{input.partOne}<br/><br/>{input.partTwo}</p>
+                  <motion.h1 className={styles.title}
+                    initial={{ scale: .8, y: "-10rem", opacity: 0}}
+                    animate={{ scale: 1.2, y: 0, opacity: 1}}
+                    transition={{ ease: "easeOut", duration: 1 }}
+                  >{input.title}</motion.h1>
+                  < motion.p className={styles.paragraaf}
+                    initial={{ y: "-5rem", opacity: 0 }}
+                    animate={{ y: 0, opacity: 1}}
+                    transition={{ ease: "easeInOut", duration: 1, delay: .5 }}
+                  >{input.partOne}<br/><br/>{input.partTwo}</motion.p>
 
                   <Link href="/campagne">
-                      <a className={styles.button}>Meer informatie</a>
+                      <motion.a className={styles.button}
+                         initial={{ y: "-5rem", opacity: 0 }}
+                         animate={{ y: 0, opacity: 1}}
+                         transition={{ ease: "easeInOut", duration: 1.5, delay: 1.5 }}
+                      >Meer informatie</motion.a>
                   </Link>
               </div>
 
-              <div className={styles.share}>
+              <motion.div className={styles.share}
+                initial={{ y: "20rem", opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ ease: "easeOut", duration: 2, delay: 1.25 }}
+              >
                   <p className={styles.share_tagline}>Share with your friends</p>
                   <div className={styles.share_image}>
                       <Image 
@@ -31,7 +48,7 @@ export default function Bubble ({ input }) {
                           height={51}
                       />
                   </div>
-              </div>
+              </motion.div>
           </article>
       </section>
     )
