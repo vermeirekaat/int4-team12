64d0b881675a8dfc83e14a184b0188eaa6097644
Kaat Vermeire <kaat.vermeire@student.howest.be>
2021-06-21 16:32:15 +0200

    implement hover and other design of examples close #58

--- components/campagne/Examples.js
+++ components/campagne/Examples.js
@@ -19,16 +19,10 @@ export default function Examples () {
                     <div className={styles.example_header}>
                         <h3 className={styles.trump_title}> Verkiezingen Trump 2016</h3>
                         <div className={styles.trump_image}>
-                            <Image
-                                src="/assets/campagne/trump.png"
-                                alt="Trump"
-                                width={200}
-                                height={240}
-                            />
                         </div>
                     </div>
                     <div className={styles.trump_content}>
-                        <p className={styles.example_paragraaf}>Niemand had verwacht dat Trump de nieuwe president zou worden in 2016, en toch werd dit realiteit. <br /> <br />De verklaring hiervoor is 'Trumpism' waarbij Trump-aanhangers online gesteund werden in hun opninies. <br />Door de filter bubble kregen ze geen andere perspectieven te zien waardoor ze enkel extremer gingen denken - en uiteindelijk ook stemmen.</p>
+                        <p className={styles.example_paragraaf}>Niemand had verwacht dat Trump de nieuwe president zou worden in 2016, en toch werd dit realiteit. <br/> <br/>De verklaring hiervoor is 'Trumpism' waarbij Trump-aanhangers online gesteund werden in hun opninies. <br />Door de filter bubble kregen ze geen andere perspectieven te zien waardoor ze enkel extremer gingen denken - en uiteindelijk ook stemmen.</p>
                     </div>
                 </article>
 
@@ -36,12 +30,6 @@ export default function Examples () {
                     <div className={styles.example_header}>
                         <h3 className={styles.boris_title}> Brexit Referendum 2016</h3>
                         <div className={styles.boris_image}>
-                            <Image
-                                src="/assets/campagne/boris.png"
-                                alt="Boris"
-                                width={200}
-                                height={240}
-                            />
                         </div>
                     </div>
                     <div className={styles.boris_content}>
