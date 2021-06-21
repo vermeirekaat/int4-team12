4c861f5fd2104f8aafcc7e4b9616c9830570d713
Kaat Vermeire <kaat.vermeire@student.howest.be>
2021-06-21 12:40:07 +0200

    adding rectangles #57

--- components/campagne/Header.js
+++ components/campagne/Header.js
@@ -5,16 +5,22 @@ export default function Footer () {
 
     return (
         <section className={styles.header}>
-            <h2 className={styles.headline}>
-                        <span>Warning :</span> dit was geen echte stemtest
-                    </h2>
+             <h2 className={styles.hidden}>Campagne - Filter Bubbles</h2>
+                <div className={styles.header_title}>
+                    <p className={styles.warning}>Warning</p>
+                    <p className={styles.headline}>Dit was geen echte stemtest</p>
+                </div>
+               
+           
                     <div className={styles.introduction}>
                         <div className={styles.window_green}></div>
                         <p className={styles.paragraaf}>
                             De stemtest die je zonet heb ingevuld, was niet echt.<br /> <br /> Dit heb je misschien zelf gemerkt, toen je geen controle meer had over je antwoorden. Geen paniek, dit was juist de bedoeling.<br/><br/>Hiermee wordt de invloed van een filter bubble geïllustreerd.
                         </p>
                     </div>
-                    <div className={styles.image_protest}>
+                    <div className={styles.rectOne}></div>
+                    <div className={styles.rectTwo}></div>
+                    {/*<div className={styles.image_protest}>
                         <Image
                             src="/assets/campagne/protest.png"
                             alt="Protest"
@@ -28,7 +34,7 @@ export default function Footer () {
                             alt="Phone with emoticons"
                             layout="fill"
                         />
-                    </div>
+                    </div>*/}
         </section>
     )
 }
